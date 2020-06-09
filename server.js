4e9c73a3f0577d5ea8ff509c1082305e8b6a564a
Steve Klabnik <steve@steveklabnik.com>
2020-06-09 10:24:43 -0500

    let's go back to the example they gave

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 // https://www.raygesualdo.com/posts/2017/05/09/301-redirects-with-nextjs/
+// const express = require('express')
+// 
+// const port = parseInt(process.env.PORT, 10) || 3000
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = express()
+// 
+// const redirects = [
+//   { from: '/posts/2009-12-02-about-crows', to: 'https://steveklabnik.com/writing/about-crows' },
+//   { from: '/old-link-2', to: 'https://externalsite.com/new-link-2' },
+// ]
+// 
+// redirects.forEach(({ from, to, type = 301, method = 'get' }) => {
+//   app[method](from, (req, res) => {
+//     res.redirect(type, to)
+//   })
+// })
+// 
+// app.listen(port, err => {
+//   if (err) throw err
+//   console.log(`> Ready on http://localhost:${port}`)
+// })
 const express = require('express')
+const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = express()
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-const redirects = [
-  { from: '/posts/2009-12-02-about-crows', to: 'https://steveklabnik.com/writing/about-crows' },
-  { from: '/old-link-2', to: 'https://externalsite.com/new-link-2' },
-]
+app.prepare().then(() => {
+  const server = express()
 
-redirects.forEach(({ from, to, type = 301, method = 'get' }) => {
-  app[method](from, (req, res) => {
-    res.redirect(type, to)
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/a', req.query)
   })
-})
 
-app.listen(port, err => {
-  if (err) throw err
-  console.log(`> Ready on http://localhost:${port}`)
+  server.get('/b', (req, res) => {
+    return app.render(req, res, '/b', req.query)
+  })
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
