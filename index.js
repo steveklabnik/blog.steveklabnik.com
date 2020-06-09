a8895203b6d27c317d70fc0d1d8249aad01b44fa
Steve Klabnik <steve@steveklabnik.com>
2020-06-09 09:46:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// https://www.raygesualdo.com/posts/2017/05/09/301-redirects-with-nextjs/
+const express = require('express')
+const next = require('next')
+const { join } = require('path')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const redirects = [
+  { from: '/posts/2009-12-02-about-crows', to: 'https://steveklabnik.com/writing/about-crows' },
+  { from: '/old-link-2', to: 'https://externalsite.com/new-link-2' },
+]
+
+app.prepare().then(() => {
+  const server = express()
+
+  redirects.forEach(({ from, to, type = 301, method = 'get' }) => {
+    server[method](from, (req, res) => {
+      res.redirect(type, to)
+    })
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, err => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
