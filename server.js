e6aa8bcd9e53340246ad4eae100572fcf035e93b
Steve Klabnik <steve@steveklabnik.com>
2020-06-09 10:08:28 -0500

    what if we respect the port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // https://www.raygesualdo.com/posts/2017/05/09/301-redirects-with-nextjs/
 const express = require('express')
 const next = require('next')
-const { join } = require('path')
 
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -22,12 +22,11 @@ app.prepare().then(() => {
   })
 
   server.get('*', (req, res) => {
-    console.log(req)
     return handle(req, res)
   })
 
-  server.listen(3000, err => {
+  server.listen(port, err => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
